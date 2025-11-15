224b914508d32c24c21d9faa844141c2685641b7
jamalnazari <jamalnazari15@gmail.com>
2025-11-15 23:32:14 +0330

    LocalStorage
    تنظیم LocalStorage برای روزهای هفته

--- my/index.js
+++ my/index.js
@@ -5,12 +5,27 @@ const pElem=$.getElementById("pElem");
 const textElem=$.getElementById("inptext");
 const timeElem=$.getElementById("inptime");
 const DisElem=$.getElementById("inpDis");
+const inpsElem=$.getElementById("inps");
+const btnelem=$.getElementById("btnElem")
 
-
+let array=[]
 
 dayElems.forEach((i)=>{
     i.addEventListener('click' , ()=>{
-       pElem.innerHTML= i.innerHTML
+
+      inpsElem.innerHTML=""
+      
+
+       pElem.innerHTML=i.innerHTML;
+       pElem.id=i.id;
+
+       let mored=localStorage.getItem(pElem.id)
+       array=[]
+       if(mored){
+         array=JSON.parse(mored)
+         gener(array)
+       }
+
     })
 })
 
@@ -26,6 +41,56 @@ day.addEventListener('click', () => {
     });
   });
 
-  const addplan=()=>{
-     
-  }
+  
+
+const addplan=()=>{
+
+         if(textElem.value==="" || pElem.innerHTML===""){
+            alert("Write your plan and choose your day")
+         }else{
+      
+            let inpvalue={
+               title:textElem.value,
+               time:timeElem.value,
+               Discription:DisElem.value
+            }
+            array.push(inpvalue)
+            gener(array)
+            save(array)
+            textElem.value=""
+            timeElem.value=""
+            DisElem.value=""
+         }
+      
+
+}
+
+const save=(varray)=>{
+    localStorage.setItem(pElem.id , JSON.stringify(varray))
+   }
+
+const gener=(vrodi)=>{
+      inpsElem.innerHTML=""
+      vrodi.forEach((i)=>{
+
+         let parentElem=$.createElement("div")
+         let newElem1=$.createElement("p");
+         let newElem2=$.createElement("p");
+         let newElem3=$.createElement("p");
+
+         newElem1.innerHTML=i.title;
+         newElem2.innerHTML=i.time;
+         newElem3.innerHTML=i.Discription;
+         
+         parentElem.append(newElem1)  
+         parentElem.append(newElem2)  
+         parentElem.append(newElem3)  
+         parentElem.classList.add("parentElem")
+         inpsElem.append(parentElem)
+
+      })
+
+   }
+
+
+   btnelem.addEventListener("click" , addplan)

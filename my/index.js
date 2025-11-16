af0b02fc41b0103488619d71840f5f29422f922b
jamalnazari <jamalnazari15@gmail.com>
2025-11-16 11:50:56 +0330

    Delet-button , css
    برای هر ]یتم کلید حذف ست شد.
    تنظیمات ظاهری هر آیتم انجام شد

--- my/index.js
+++ my/index.js
@@ -71,24 +71,40 @@ const save=(varray)=>{
 
 const gener=(vrodi)=>{
       inpsElem.innerHTML=""
-      vrodi.forEach((i)=>{
+      vrodi.forEach((i , index)=>{
 
          let parentElem=$.createElement("div")
          let newElem1=$.createElement("p");
          let newElem2=$.createElement("p");
          let newElem3=$.createElement("p");
-
+         let newElem4=$.createElement("p")
+         
          newElem1.innerHTML=i.title;
          newElem2.innerHTML=i.time;
          newElem3.innerHTML=i.Discription;
+         newElem4.innerHTML="Delet";
          
+         parentElem.append(newElem4)
          parentElem.append(newElem1)  
          parentElem.append(newElem2)  
          parentElem.append(newElem3)  
+         
+         newElem4.classList.add("p-btn")
          parentElem.classList.add("parentElem")
          inpsElem.append(parentElem)
 
+         newElem4.addEventListener("click" , function(){
+
+            array.splice(index, 1)
+            save(array)
+            gener(array)
+
+            
+         })
+
       })
+
+
 
    }
 
